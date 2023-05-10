1bd73016cf30469ebde0df50598257e683192766
kaiser54 <agboolatemitope070@gmail.com>
2023-05-10 19:30:18 +0100

    Finished working on the date for tab for desktop view

--- pages/dashboard/history.vue
+++ pages/dashboard/history.vue
@@ -14,34 +14,133 @@
             {{ tab }}
           </div>
         </div>
-        <div>
-          <label for="start-date">Start Date:</label>
-          <input
-            class="input"
-            type="date"
-            id="start-date"
-            v-model="startDate"
-            placeholder="MM-DD-YYYY"
-          />
-
-          <label for="end-date">End Date:</label>
-          <input
-            class="input"
-            type="date"
-            id="end-date"
-            v-model="endDate"
-            placeholder="MM-DD-YYYY"
-          />
-
-          <button @click="handleFilter">Filter</button>
+        <div class="filter-dates">
+          <div class="datepicker-toggle">
+            <p>From</p>
+            <!-- <span class="datepicker-toggle-button"></span> -->
+            <button class="datepicker-toggle-button">
+              <span v-if="startDate">{{ startDate }}</span>
+              <span v-else class="btn-dsp">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="21"
+                  height="20"
+                  viewBox="0 0 21 20"
+                  fill="none"
+                >
+                  <path
+                    d="M3 4.9987C3 4.07822 3.74619 3.33203 4.66667 3.33203H16.3333C17.2538 3.33203 18 4.07822 18 4.9987V16.6654C18 17.5859 17.2538 18.332 16.3333 18.332H4.66667C3.74619 18.332 3 17.5859 3 16.6654V4.9987Z"
+                    stroke="#303237"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M3 8.33203H18"
+                    stroke="#303237"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M13.834 1.66797V5.0013"
+                    stroke="#303237"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M7.16602 1.66797V5.0013"
+                    stroke="#303237"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+                <p>YYYY-MM-DD</p>
+              </span>
+            </button>
+            <input
+              type="date"
+              class="input datepicker-input"
+              v-model="startDate"
+            />
+          </div>
+          <div class="datepicker-toggle">
+            <p>To</p>
+            <!-- <span class="datepicker-toggle-button"></span> -->
+            <button class="datepicker-toggle-button">
+              <span v-if="endDate">{{ endDate }}</span>
+              <span v-else class="btn-dsp">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="21"
+                  height="20"
+                  viewBox="0 0 21 20"
+                  fill="none"
+                >
+                  <path
+                    d="M3 4.9987C3 4.07822 3.74619 3.33203 4.66667 3.33203H16.3333C17.2538 3.33203 18 4.07822 18 4.9987V16.6654C18 17.5859 17.2538 18.332 16.3333 18.332H4.66667C3.74619 18.332 3 17.5859 3 16.6654V4.9987Z"
+                    stroke="#303237"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M3 8.33203H18"
+                    stroke="#303237"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M13.834 1.66797V5.0013"
+                    stroke="#303237"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M7.16602 1.66797V5.0013"
+                    stroke="#303237"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+                <p>YYYY-MM-DD</p>
+              </span>
+            </button>
+            <input
+              type="date"
+              class="input datepicker-input"
+              v-model="endDate"
+            />
+          </div>
         </div>
       </div>
     </div>
-    <span class="datepicker-toggle">
-      <!-- <span class="datepicker-toggle-button"></span> -->
-      <button class="btn primay-btn datepicker-toggle-button">START DATEEEEEEEEEEEE</button>
-      <input type="date" class="datepicker-input" />
-    </span>
+    <div>
+      <label for="start-date">Start Date:</label>
+      <input
+        class="input"
+        type="date"
+        id="start-date"
+        v-model="startDate"
+        placeholder="MM-DD-YYYY"
+      />
+
+      <label for="end-date">End Date:</label>
+      <input
+        class="input"
+        type="date"
+        id="end-date"
+        v-model="endDate"
+        placeholder="MM-DD-YYYY"
+      />
+
+      <button @click="handleFilter">Filter</button>
+    </div>
     <ul>
       <li v-for="item in filteredData" :key="item.id">{{ item.name }}</li>
     </ul>
@@ -123,22 +222,56 @@ export default {
 
 <style scoped>
 .datepicker-toggle {
-  display: inline-block;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 18px;
   position: relative;
 }
+.datepicker-toggle p {
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  /* identical to box height, or 150% */
+
+  /* Grey/Grey1 */
+
+  color: var(--grey-grey1);
+}
 .datepicker-toggle-button {
-  position: absolute;
+  /* position: absolute;
   left: 0;
-  top: 0;
-  width: 180px;
-  height: 40px;
-  /* background-image: url('@/static/category2.png'); */
+  top: 0; */
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 16px;
+  gap: 8px;
+
+  /* max-width: 155px; */
+  min-width: 155px;
+  /* width: 100%; */
+  height: 48px;
+
+  /* White */
+
+  background: var(--white);
+  /* Grey/Grey4 */
+
+  border: 1px solid var(--grey-grey4);
+  border-radius: 100px;
+}
+.datepicker-toggle .input {
+  background: none !important;
+  border: none;
 }
 .datepicker-input {
   position: absolute;
   left: 0;
   top: 0;
-  width: 180px;
+  width: 155px;
   height: 40px;
   opacity: 0;
   cursor: pointer;
@@ -154,16 +287,27 @@ export default {
   padding: 0;
   cursor: pointer;
 }
+.btn-dsp {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+}
+.btn-dsp p {
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 21px;
+  /* identical to box height, or 150% */
 
+  /* Grey/Grey1 */
 
-
-
-
-
+  color: var(--grey-grey1);
+}
 
 .filter-head {
   display: flex;
   justify-content: space-between;
+  width: 100%;
 }
 .filter-tabs {
   display: flex;
@@ -171,13 +315,22 @@ export default {
   align-items: center;
   gap: 8px;
 }
+.filter-dates {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 0px;
+  gap: 16px;
+  position: relative;
+}
 .tab.clicked {
   /* Accent/A75 */
 
   background: var(--accent-a75);
   /* Accent/A200 */
 
-  outline: 1px solid var(--accent-a200);
+  border: 1px solid var(--accent-a200);
 }
 </style>
 <style>
