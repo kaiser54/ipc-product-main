b0f33bc3ab3489f4ea8fa1db72cb66530fd2884a
kaiser54 <agboolatemitope070@gmail.com>
2023-06-02 12:23:00 +0100

    update the userHeaderComponent

--- components/userHeaderComponent.vue
+++ components/userHeaderComponent.vue
@@ -2,49 +2,23 @@
   <div class="user-details-component animate__animated animate__slideInRight">
     <div class="component-header">
       <div class="component-header-main">
-        <svg
-          @click="$emit('redirectToprofilepage')"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-        >
-          <path
-            d="M3 12L10 19M21 12H3H21ZM3 12L10 5L3 12Z"
-            stroke="#565C69"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
+        <svg @click="$emit('redirectToprofilepage')" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+          viewBox="0 0 24 24" fill="none">
+          <path d="M3 12L10 19M21 12H3H21ZM3 12L10 5L3 12Z" stroke="#565C69" stroke-width="2" stroke-linecap="round"
+            stroke-linejoin="round" />
         </svg>
         <p>Profile</p>
         <div class="component-cart">
-          <div class="badge">
-            <p>9</p>
-          </div>
           <nuxt-link to="/dashboard/market/cart">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-            >
+            <div class="badge" v-if="cart.length > 0">
+              <p>{{ getCartLength }}</p>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
               <path
                 d="M3 9C3 7.89543 3.89543 7 5 7H19C20.1046 7 21 7.89543 21 9V20C21 21.1046 20.1046 22 19 22H5C3.89543 22 3 21.1046 3 20V9Z"
-                stroke="#565C69"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M8 10V6C8 3.79086 9.79086 2 12 2C14.2091 2 16 3.79086 16 6V9.6888"
-                stroke="#565C69"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
+                stroke="#565C69" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+              <path d="M8 10V6C8 3.79086 9.79086 2 12 2C14.2091 2 16 3.79086 16 6V9.6888" stroke="#565C69"
+                stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
             </svg>
           </nuxt-link>
         </div>
@@ -54,25 +28,17 @@
       <userNameMail />
       <div class="flex">
         <div class="acc-btn">
-          <accountDetails
-            :BusinessName="BusinessName"
-            :user-name="userName"
-            :phone-numbers="phoneNumbers"
-            :invalidNumber="invalidNumber"
-            :show-new-phone-number="showNewPhoneNumber"
-            @add-number="handleAddNumber"
-          />
+          <accountDetails :BusinessName="BusinessName" :user-name="userName" :phone-numbers="phoneNumbers"
+            :invalidNumber="invalidNumber" :show-new-phone-number="showNewPhoneNumber" @add-number="handleAddNumber" />
         </div>
       </div>
-      <addNumberFunc
-        v-if="addNumberFunc"
-        @toggleAddNumberFunc="toggleAddNumberFunc"
-      />
+      <addNumberFunc v-if="addNumberFunc" @toggleAddNumberFunc="toggleAddNumberFunc" />
     </div>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
 import "animate.css";
 export default {
   data() {
@@ -91,6 +57,13 @@ export default {
       BusinessName: "Chicken Republic",
     };
   },
+    computed: {
+      ...mapState(['cart']),
+      getCartLength() {
+        const cartLength = this.$store.state.cart.length;
+        return cartLength
+      }
+    },
   methods: {
     toggleAddNumberFunc() {
       this.addNumberFunc = !this.addNumberFunc;
@@ -124,6 +97,7 @@ export default {
   /* max-width: 575px; */
   width: auto;
 }
+
 .acc-btn {
   width: 100%;
 }
