07b0353ffde430095b84c8e42c59528438248057
Agboola Temitope <85450650+kaiser54@users.noreply.github.com>
2024-02-14 12:13:54 +0100

    fxnality

--- pages/dashboard/track_orders/index.vue
+++ pages/dashboard/track_orders/index.vue
@@ -441,9 +441,6 @@ export default {
     try {
       const response = await this.$axios.get(`/orders/customer/${userId}`);
       this.order = response?.data?.data?.orders;
-      console.log('order',this.order);
-      this.products = response?.data?.data?.orders?.products;
-      console.log('products',this.products);
       this.loading = false;
     } catch (error) {
       console.error("Error fetching order details:", error);
@@ -479,9 +476,7 @@ export default {
         const response = await this.$axios.get(`/orders/customer/${userId}`);
         this.loading = false;
         this.order = response?.data?.data?.orders;
-        (this.order);
         this.products = response?.data?.data?.orders[0]?.products;
-        (this.products);
         this.loading = false;
       } catch (error) {
         this.loading = true;
